87cc9e69cbcd3a156d4b9238847dfd79f5713b0e
Pitz <pitz.eduardo@gmail.com>
2019-09-12 18:02:03 -0300

    Atualizando dependências e salvando userId

---
+++ client/build/precache-manifest.af753e08a2208ee32fba0cf12b9790ed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc41607ddd3c591f6481b2995500cc21",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c3e9054ff0c682ca335f",
+    "url": "/static/css/main.7afc6e91.chunk.css"
+  },
+  {
+    "revision": "6456b575a6cbf527a2a2",
+    "url": "/static/js/2.a603e126.chunk.js"
+  },
+  {
+    "revision": "c3e9054ff0c682ca335f",
+    "url": "/static/js/main.daba21a3.chunk.js"
+  },
+  {
+    "revision": "b3938888db487c1703c6",
+    "url": "/static/js/runtime~main.a7ca617d.js"
+  }
+]);
