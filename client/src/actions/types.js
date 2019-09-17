5b1cd2723c9a21ae85dc171ea0997ec3d8125c92
Pitz <pitz.eduardo@gmail.com>
2019-09-17 08:12:52 -0300

    Testing

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -6,4 +6,4 @@ export const CREATE_STREAM     = 'CREATE_STREAM';
 export const FETCH_STREAM_LIST = 'FETCH_STREAM_LIST';
 export const FETCH_STREAM      = 'FETCH_STREAM';
 export const DELETE_STREAM     = 'DELETE_STREAM';
-export const EDIT_STREAM       = 'EDIT_STREAM';
+export const EDIT_STREAM      = 'EDIT_STREAM';
