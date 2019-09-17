00a3e3ab514d710613a1e1853b76c645b93ad6fe
Pitz <pitz.eduardo@gmail.com>
2019-09-17 07:43:42 -0300

    Adicionando Types REST para Stream.

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,4 +1,10 @@
-import { SIGN_IN, SIGN_OUT } from './types';
+import { SIGN_IN, 
+         SIGN_OUT, 
+         CREATE_STREAM,
+         FETCH_STREAM_LIST,
+         FETCH_STREAM,
+         DELETE_STREAM,
+         EDIT_STREAM } from './types';
 import streams from '../apis/streams';
 
 export const signIn = (userId) => {
@@ -16,6 +22,35 @@ export const signOut = () => {
 
 export const createStream = (formValues) => {
     return async (dispatch) => {
-        streams.post('/streams', formValues);
+        const response = await streams.post('/streams', formValues);
+        dispatch({ type: CREATE_STREAM, payload: response.data })
+    };
+};
+
+export const fetchStreamList = () => {
+    return async (dispatch) => {
+        const response = await streams.get('/streams');
+        dispatch({ type: FETCH_STREAM_LIST, payload: response.data })
+    };
+};
+
+export const fetchStream = (id) => {
+    return async (dispatch) => {
+        const response = await streams.get(`/streams/${id}`);
+        dispatch({ type: FETCH_STREAM, payload: response.data })
+    };
+};
+
+export const deleteStream = (id) => {
+    return async (dispatch) => {
+        await streams.get(`/streams/${id}`);
+        dispatch({ type: DELETE_STREAM, payload: id })
+    };
+};
+
+export const editStream = (id, formValues) => {
+    return async (dispatch) => {
+        const response = await streams.put(`/streams/${id}`, formValues);
+        dispatch({ type: EDIT_STREAM, payload: response.data })
     };
 };
